a9db7018d3f46b1f6f00c7fb1717f6c2e0924154
PrestonMcIllece <mcillece.p@gmail.com>
2020-03-24 12:43:06 -0600

    Added domain to ALLOWED_HOSTS

--- senior-project/settings.py
+++ senior-project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yg!gh21i!5$nm8bttt288q9)2@k5mdm4&0nubbq@2mq$io5563'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.fplpredictions.com']
 
 
 # Application definition
