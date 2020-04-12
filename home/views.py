2d80e4ff140d890e9fe540de4d5039162dd9f1a9
PrestonMcIllece <mcillece.p@gmail.com>
2020-04-11 20:36:18 -0600

    Implemented tests and subsequently refactored some code

--- home/views.py
+++ home/views.py
@@ -24,7 +24,20 @@ def all_players(request):
     return render(request, 'home/all-players.html', {'players': players})
 
 def build_team(request):
-    best_players = calculate_best_players()
+    best_players_tuple = calculate_best_players()
+    best_players_list = best_players_tuple[0]
+    best_players_dict = best_players_tuple[1]
+
+    best0, best1, best2, best3, best4 = best_players_list[0], best_players_list[1], best_players_list[2], best_players_list[3], best_players_list[4]
+    best5, best6, best7, best8, best9 = best_players_list[5], best_players_list[6], best_players_list[7], best_players_list[8], best_players_list[9]
+    best10, best11, best12, best13, best14 = best_players_list[10],  best_players_list[11], best_players_list[12], best_players_list[13], best_players_list[14]
+
+    player0, player1, player2, player3, player4 = best_players_dict[best0], best_players_dict[best1], best_players_dict[best2], best_players_dict[best3], best_players_dict[best4]
+    player5, player6, player7, player8, player9 = best_players_dict[best5], best_players_dict[best6], best_players_dict[best7], best_players_dict[best8], best_players_dict[best9]
+    player10, player11, player12, player13, player14 = best_players_dict[best10], best_players_dict[best11], best_players_dict[best12], best_players_dict[best13], best_players_dict[best14]
+
+    best_players = "The 15 best possible players are: " + player0 + ", " + player1 + ", " + player2 + ", " + player3 + ", " + player4 + ", " + player5 + ", " + player6 + ", " + player7 + ", " + player8 + ", " + player9 + ", " + player10 + ", " + player11 + ", " + player12 + ", " + player13 + ", and " + player14 + ". Good luck trying to fit them all into your team!"
+
     return render(request, 'home/best-team.html', {'best_players': best_players})
 
 def compare_players(request):
@@ -90,8 +103,6 @@ def suggest_players(request):
         return render(request, 'home/team-results.html', {'suggestions': suggestions, 'failed_player_entries': failed_player_entries})
 
 
-
-
 # Helper methods below this line #
 
 def calculate_best_players():
@@ -99,20 +110,11 @@ def calculate_best_players():
     predicted_scores = run_model(json_object)
     keys = list(predicted_scores)
     keys.sort(reverse = True)
-    best0, best1, best2, best3, best4 = keys[0], keys[1], keys[2], keys[3], keys[4]
-    best5, best6, best7, best8, best9 = keys[5], keys[6], keys[7], keys[8], keys[9]
-    best10, best11, best12, best13, best14 = keys[10],  keys[11], keys[12], keys[13], keys[14]
-
-    player0, player1, player2, player3, player4 = predicted_scores[best0], predicted_scores[best1], predicted_scores[best2], predicted_scores[best3], predicted_scores[best4]
-    player5, player6, player7, player8, player9 = predicted_scores[best5], predicted_scores[best6], predicted_scores[best7], predicted_scores[best8], predicted_scores[best9]
-    player10, player11, player12, player13, player14 = predicted_scores[best10], predicted_scores[best11], predicted_scores[best12], predicted_scores[best13], predicted_scores[best14]
-
-    return "The 15 best possible players are: " + player0 + ", " + player1 + ", " + player2 + ", " + player3 + ", " + player4 + ", " + player5 + ", " + player6 + ", " + player7 + ", " + player8 + ", " + player9 + ", " + player10 + ", " + player11 + ", " + player12 + ", " + player13 + ", and " + player14 + ". Good luck trying to fit them all into your team!"
+    return (keys[:15], predicted_scores)
     
 def calculate_comparisons(inputted_team):
     players_list= []
     json_object = connect(APIS[0])
-    confidence_level = ''
     secondPlayer = False
     for inputted_player in inputted_team:
         for person in json_object:
@@ -124,28 +126,38 @@ def calculate_comparisons(inputted_team):
                 else:
                     secondPlayer = True
                     first_player_id = person['id']
-    predicted_scores = run_model(players_list)
-    if (len(list(predicted_scores)) != 2):
+    if (len(players_list) != 2):
         return '-1'
     else:
+        predicted_scores = run_model(players_list)
         player1_pred_score, player2_pred_score = list(predicted_scores.keys())[0], list(predicted_scores.keys())[1]
         adj_p1_pred_score, adj_p2_pred_score = player1_pred_score / 38, player2_pred_score / 38
 
         player_form_tuple = asyncio.run(get_form(player_id_tuple))
 
+        #player1
         if player_form_tuple[0] > 3.0:
             adj_p1_pred_score = adj_p1_pred_score * 2
         elif player_form_tuple[0] == 0.0:
             adj_p1_pred_score = adj_p1_pred_score * 0.5
 
-        if abs(adj_p1_pred_score - adj_p2_pred_score) < 1.5:
-            confidence_level = 'only slightly confident'
-        elif abs(adj_p1_pred_score - adj_p2_pred_score) < 3.0:
-            confidence_level = 'moderately confident'
-        else:
-            confidence_level = 'extremely confident'
+        #player2
+        if player_form_tuple[1] > 3.0:
+            adj_p2_pred_score = adj_p2_pred_score * 2
+        elif player_form_tuple[1] == 0.0:
+            adj_p2_pred_score = adj_p2_pred_score * 0.5
 
-        return "We are " + confidence_level + ' that ' + predicted_scores[max(player1_pred_score, player2_pred_score)] +  " will outpeform " + predicted_scores[min(player1_pred_score, player2_pred_score)] + " this week."
+        return 'We are ' + confidence_level((adj_p1_pred_score, adj_p2_pred_score)) + ' that ' + predicted_scores[max(player1_pred_score, player2_pred_score)] +  ' will outperform ' + predicted_scores[min(player1_pred_score, player2_pred_score)] + ' this week.'
+
+def confidence_level(adj_score_tuple):
+    confidence = ''
+    if abs(adj_score_tuple[0] - adj_score_tuple[1]) < 1.5:
+        confidence = 'only slightly confident'
+    elif abs(adj_score_tuple[0] - adj_score_tuple[1]) < 3.0:
+        confidence = 'moderately confident'
+    else:
+        confidence = 'extremely confident'
+    return confidence
 
 def format_name(text):
     try:
@@ -196,5 +208,5 @@ def parse_players(inputted_team):
         second_worst_player_name = player_score_predictions[second_worst_player_score]
         third_worst_player_name = player_score_predictions[third_worst_player_score]
 
-        return_tuple =  ("Your worst predicted players in order are " + worst_player_name + ", " + second_worst_player_name + ", and " + third_worst_player_name + ". You should consider benching these players or finding replacements on the transfer market.", 15 - len(players_list))
+        return_tuple =  ("Your worst predicted players in order are " + worst_player_name + ", " + second_worst_player_name + ", and " + third_worst_player_name + ". You should consider benching these players or finding replacements on the transfer market.", 15 - len(keys))
         return return_tuple
